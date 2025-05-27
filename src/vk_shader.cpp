417a0f4708db90025dd0c320c1ab8c751f213013
kananniftiyev <kananniftiyev010@gmail.com>
2025-05-27 15:56:06 +0200

    refactor: vk_shader

--- src/vk_shader.cpp
+++ src/vk_shader.cpp
@@ -6,18 +6,20 @@ VkShaderModule vk_boost::shader::createShaderModule(const VkDevice &device, cons
 
   std::ifstream input_shader(spv_path, std::ios::ate | std::ios::binary);
 
-  if (!input_shader)
+  if (!input_shader.is_open())
   {
     return VK_NULL_HANDLE;
   }
 
-  size_t size = static_cast<uint32_t>(input_shader.tellg());
-  if (size % sizeof(uint32_t) != 0)
+  std::streamsize file_size = input_shader.tellg();
+  if (file_size <= 0 || file_size % sizeof(uint32_t) != 0)
   {
     return VK_NULL_HANDLE;
   }
 
+  size_t size = static_cast<size_t>(file_size);
   std::vector<uint32_t> spv_code(size / sizeof(uint32_t));
+
   input_shader.seekg(0);
   input_shader.read(reinterpret_cast<char *>(spv_code.data()), size);
 
@@ -28,7 +30,12 @@ VkShaderModule vk_boost::shader::createShaderModule(const VkDevice &device, cons
   info.pCode = spv_code.data();
 
   VkShaderModule shader_module;
-  vkCreateShaderModule(device, &info, nullptr, &shader_module);
+  auto res = vkCreateShaderModule(device, &info, nullptr, &shader_module);
+
+  if (res != VK_SUCCESS)
+  {
+    return VK_NULL_HANDLE;
+  }
 
   return shader_module;
 }
